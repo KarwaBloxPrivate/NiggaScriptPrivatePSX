6912119040d1d40feba9d4afccd644fd47954592
KarwaBloxPrivate <128983526+KarwaBloxPrivate@users.noreply.github.com>
2023-03-26 16:31:20 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -122,7 +122,7 @@ function ServerHop()
 			elseif typeof(v.ping) == "table" and typeof(v.ping.total) == "number" then
 				ping = v.ping.total
 			end
-			if ping ~= nil then
+			if ping ~= nil and ping > 30 then
 				table.insert(Servers, {ping = ping, server = v})
 			end
 		end
